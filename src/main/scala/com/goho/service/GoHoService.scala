a80a2efe5331bf2d19838698f5b6ddd29c189dd9
Yash Datta <Yash.Datta@guavus.com>
2016-03-31 10:29:37 +0530

    GOHO-006: Use goho execution context

--- src/main/scala/com/goho/service/GoHoService.scala
+++ src/main/scala/com/goho/service/GoHoService.scala
@@ -22,56 +22,25 @@ object GoHoService extends AuthorizeKey {
   HotelDataBase.init()
 
   val authHeader = CaseInsensitiveString("Authorization")
-  // A Router can mount multiple services to prefixes.  The request is passed to the
-  // service with the longest matching prefix.
-  def service(implicit executionContext: ExecutionContext = ExecutionContext.global): HttpService = Router(
-    "" -> gohoService
-  )
 
   def gohoService(implicit executionContext: ExecutionContext = gohoExecutorService) = HttpService {
     case req @ GET -> Root =>
       Ok("")
     case req @ GET -> Root / "getHotelsByCity" / city => {
-      //val city = req.attributes.get
       req.authType match {
         case p @ Some(AuthScheme.Bearer) =>
           val key = req.headers.get(authHeader).get.value.split(" ").last
           if (accept(key)) {
-            val records = Task.now(HotelDataBase.getOrderedRecords(city)).run
+            val records = TaskFactory.getTask(HotelDataBase.getOrderedRecords(city)).run
             val output = records.mkString("\n")
             Ok(s"${output}")
           } else {
             Task.now(Response(Status.Unauthorized).putHeaders(Authorization(OAuth2BearerToken(key))))
-            //Unauthorized(Challenge("", "", Map("Authorization"->key)))
           }
-        case None =>
+        // Does not entertain requests for any other auth schemes for now
+        case Some(_) | None =>
           Forbidden("")
       }
-      /*if (req.authType == None) {
-        Forbidden("")
-      } else {
-
-        //val key = bearer.get.value.split(" ").last
-        if (accept(key)) {
-          val records = Task.now(HotelDataBase.getOrderedRecords(city)).run
-          val output = records.mkString("\n")
-          Ok(s"${output}")
-        } else {
-          Task.now(Response(Status.Unauthorized).putHeaders(`WWW-Authenticate`(Challenge("", "", Map("Authorization"->key)))))
-          //Unauthorized(Challenge("", "", Map("Authorization"->key)))
-        }
-      } */
     }
   }
-
-  // Services can be protected using HTTP authentication.
-  /*val realm = "testrealm"
-
-  def auth_store(r: String, u: String) = if (r == realm && u == "username") Task.now(Some("password"))
-  else Task.now(None)
-
-  val digest = new DigestAuthentication(realm, auth_store)
-
-  def gohoService = digest(basicService)*/
-
 }
